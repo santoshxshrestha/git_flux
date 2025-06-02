3a6f4126eabe9f9029fc7f73284f56124dc3101a
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-06-02 17:31:48 +0545

    description

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,43 @@
-#![allow(unused)]
-use clap::Parser;
-use reqwest::header::{HeaderMap, HeaderValue, AUTHORIZATION, USER_AGENT};
+use reqwest::header::{ACCEPT, USER_AGENT};
+use std::error::Error;
 
-pub fn build_github_headers(token: &str) -> HeaderMap {
-    let mut headers = HeaderMap::new();
-    headers.insert(
-        AUTHORIZATION,
-        HeaderValue::from_str(&format!("token {}", token)).expect("Invalid token"),
-    );
-    headers.insert(USER_AGENT, HeaderValue::from_static("my-github-cli/0.1"));
-    headers
-}
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let owner = "santoshxshrestha";
+    let repo = "nexish";
 
-#[derive(Parser)]
-struct Args {
-    #[arg(short, long)]
-    auth: String,
-}
+    // GitHub API endpoint
+    let url = format!("https://api.github.com/repos/{owner}/{repo}/commits?per_page=1");
 
-fn main() {
-    println!("this is main function");
+    let client = reqwest::Client::new();
+    let res = client
+        .get(&url)
+        .header(USER_AGENT, "rust-lang reqwest")
+        .header(ACCEPT, "application/vnd.github.v3+json")
+        .send()
+        .await?;
+
+    if !res.status().is_success() {
+        eprintln!("Failed to fetch commits: {}", res.status());
+        return Ok(());
+    }
+
+    // GitHub returns pagination info in `Link` header
+    if let Some(link_header) = res.headers().get("link") {
+        let link_str = link_header.to_str()?;
+        // Find the last page URL
+        for part in link_str.split(',') {
+            if part.contains("rel=\"last\"") {
+                // extract the `page=` parameter
+                let start = part.find("page=").unwrap() + 5;
+                let end = part[start..].find('>').unwrap() + start;
+                let total_commits: usize = part[start..end].parse()?;
+                println!("Total commits: {}", total_commits);
+                return Ok(());
+            }
+        }
+    }
+
+    println!("Only one page of commits found (1 commit)");
+    Ok(())
 }
