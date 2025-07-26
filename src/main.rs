79b2eb199fa6a85d0b373402dbbcf443ca765f34
santoshxshrestha <santoshxshrestha@gmail.com>
2025-07-27 01:26:47 +0545

    added details

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,34 @@
-use reqwest::header::{ACCEPT, USER_AGENT};
-use std::error::Error;
+#![allow(unused)]
+use actix_files::Files;
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, post, web};
+use askama::Template;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let owner = "santoshxshrestha";
-    let repo = "nexish";
+#[derive(Template)]
+#[template(path = "index.html")]
+struct HelloTemplate {
+    name: String,
+    language: String,
+}
 
-    // GitHub API endpoint
-    let url = format!("https://api.github.com/repos/{owner}/{repo}/commits?per_page=1");
+#[get("/")]
+async fn hello() -> impl Responder {
+    let template = HelloTemplate {
+        name: "Santosh".to_string(),
+        language: "rust".to_string(),
+    };
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(template.render().unwrap())
+}
 
-    let client = reqwest::Client::new();
-    let res = client
-        .get(&url)
-        .header(USER_AGENT, "rust-lang reqwest")
-        .header(ACCEPT, "application/vnd.github.v3+json")
-        .send()
-        .await?;
-
-    if !res.status().is_success() {
-        eprintln!("Failed to fetch commits: {}", res.status());
-        return Ok(());
-    }
-
-    // GitHub returns pagination info in `Link` header
-    if let Some(link_header) = res.headers().get("link") {
-        let link_str = link_header.to_str()?;
-        // Find the last page URL
-        for part in link_str.split(',') {
-            if part.contains("rel=\"last\"") {
-                // extract the `page=` parameter
-                let start = part.find("page=").unwrap() + 5;
-                let end = part[start..].find('>').unwrap() + start;
-                let total_commits: usize = part[start..end].parse()?;
-                println!("Total commits: {}", total_commits);
-                return Ok(());
-            }
-        }
-    }
-
-    println!("Only one page of commits found (1 commit)");
-    Ok(())
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(Files::new("/static", "./gitflux"))
+            .service(hello)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
